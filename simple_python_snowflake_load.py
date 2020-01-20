184cc515f8b1f4c539293d0db2139928e73a3049
DanielGRRB <daniel.rae@receipt-bank.com>
2020-01-20 21:30:38 +0000

    ipynb to .py

---
+++ simple_python_snowflake_load.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import pandas as pd
+import snowflake.connector
+from snowflake.sqlalchemy import URL
+from sqlalchemy import create_engine
+
+
+df = pd.read_csv("<sample_data>.csv")
+
+engine = create_engine(
+    URL(
+        account="<Hidden>",
+        user="<Hidden>",
+        password="<Hidden>",
+        database="delivery",
+        schema="data",
+        warehouse="compute_wh",
+        role="sysadmin",
+    )
+)
+
+connection = engine.connect()
+
+df.to_sql("raw_data", con=engine, index=False, if_exists="replace", chunksize=10000)
+
+connection.close()
+engine.dispose()
+
+
+# In[ ]:
+
+
+
+
